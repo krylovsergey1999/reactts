8adcdd2ced8129dd1333b5af88f15ea4e93d14c2
Krylov Sergey <47079612+krylovsergey1999@users.noreply.github.com>
2019-12-05 14:40:01 +0300

    Updates

---
+++ precache-manifest.257625ef60910875cf74a818b7758ba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659b7679c764a8f381e074d7c4e4359c",
+    "url": "/reactts/index.html"
+  },
+  {
+    "revision": "f4c54a36122e5f75a383",
+    "url": "/reactts/static/css/main.a4ca189f.chunk.css"
+  },
+  {
+    "revision": "816e40e1c39d5ca40da0",
+    "url": "/reactts/static/js/2.9b632fb8.chunk.js"
+  },
+  {
+    "revision": "f4c54a36122e5f75a383",
+    "url": "/reactts/static/js/main.17125c12.chunk.js"
+  },
+  {
+    "revision": "2a71edd0a2abc1ea2f90",
+    "url": "/reactts/static/js/runtime-main.764b8839.js"
+  }
+]);
